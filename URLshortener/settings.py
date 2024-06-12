75d14b8840451286daf855fb83bfb6312653c9f2
Asir <asiradnan23@gmail.com>
2024-06-13 00:43:28 +0600

    Initial

--- URLshortener/settings.py
+++ URLshortener/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'URLshortener.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER':'postgres',
+        'PASSWORD': os.environ.get('pass'),
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '33023'
     }
 }
 
