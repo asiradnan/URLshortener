8bf4d54fe4eedd8d08ff35fba655d395a8bc6e90
asiradnan <asiradnan23@gmail.com>
2025-08-09 21:45:32 +0600

    create actual url redirect view and url

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('shorten/',views.shorten, name = 'shorten')
+    path('shorten/',views.shorten, name = 'shorten'),
+    path('<str:short_code>/', views.get_actual_url, name = 'get_actual_url')
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import UrlSerializer
+from .serializers import UrlSerializer, StatisticsSerializer
 from .helpers import generate_unique_code
 from rest_framework import status
-from .models import Urls
+from .models import Urls, Statistics
+from django.shortcuts import redirect
 
 @api_view(['POST'])
 def shorten(request):
@@ -17,5 +18,21 @@ def shorten(request):
     serializer = UrlSerializer(data=payload)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        stat, flag = Statistics.objects.get_or_create(pk = 1)
+        stat.total_links += 1
+        stat.active_links += 1
+        stat.save()
+        stat_serializer = StatisticsSerializer(stat)
+        data = serializer.data | stat_serializer.data
+        return Response(data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def get_actual_url(request, short_code):
+    existing = Urls.objects.filter(short_code = short_code).first()
+    if existing:
+        stat, flag = Statistics.objects.get_or_create(pk = 1)
+        stat.total_clicks += 1
+        stat.save()
+        return redirect(existing.actual_url)
+    return Response({"error":"URL not found"}, status=status.HTTP_400_BAD_REQUEST)
