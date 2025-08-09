aa0b60d2991509bdd50af55d8bf9441a192590f8
asiradnan <asiradnan23@gmail.com>
2025-08-09 20:25:15 +0600

    cleanup

--- url_shortener_backend/settings.py
+++ url_shortener_backend/settings.py
@@ -32,11 +32,11 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
+    'main',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
